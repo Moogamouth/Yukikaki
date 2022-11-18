27566e5914c40c29fe708ce7a1d54b78e7f8f098
Moogamouth <moogamouth@mail.com>
2022-11-17 21:46:23 -0500

    Edited index.js and README

--- README.md
+++ README.md
@@ -5,15 +5,12 @@ Node.js package to archive webpages, links, and sources to Arweave from URLs.
 
 ```js
 (async () => {
-	await init();
-	await archive({"url": "https://en.wikipedia.org/wiki/The_Scorpion_and_the_Frog/", "key": <insert your key here>, "i": 10, "hrefs": true, "after": 1577854800});
+    const permaloom = await new (await require("permaloom"));
+    await permaloom.archive({"url": "https://en.wikipedia.org/wiki/The_Scorpion_and_the_Frog/", "key": <your key here>, "i": 2, "hrefs": true, "after": 1577854800});
 })();
 ```
 
 # Usage
-
-## .init()
-Initialize .archive() function.
 
 ## .archive([options])
 Archives a webpage and its sources to Arweave from options.url.
@@ -26,13 +23,13 @@ URL to archive.
 `Object`
 Arweave key object of an Arweave wallet.
 
+### options.i
+`Int`
+Default is 2. Determines when to stop archiving links and sources. If i > 2, options.hrefs will automatically be set to true.
+
 ### options.hrefs
 `Bool`
-Optional. If true, archive links, links of links, and links of links of links, so on. It will stop when options.i is depleted.
-
-#### options.i
-`Int`
-Default is 2. Determines when to stop archiving links.
+Optional. If true, archive links, links of links, and links of links of links, so on. It will stop when options.i is depleted. Will automatically be set to true if i > 2.
 
 ### options.after
 `Int`
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 class Permaloom {
+
 	constructor(){
 		return Promise.resolve()
 		.then(async () => {
@@ -11,21 +12,20 @@ class Permaloom {
 			return this;
 		})
 	}
+
 	async archive(options) {
-		var hrefs = true;
-		if (options.i === undefined) {
-			hrefs = false;
-			options.i = 2;
-		}
+
+		if (options.i === undefined) options.i = 2;
+
+		if (options.i > 2) options.hrefs = true;
 	
 		if (options.i > 0) {
-			const res = await this.page.goto(options.url);
+
+			const res = await this.page.goto(`${options.url}`);
 			let contentType = res.headers()["content-type"];
 			if (contentType.includes(";")) contentType = contentType.split(";")[0];
-	
-			if (!options.html) options.html = contentType === "text/html";
-	
-			if (options.html) {
+
+			if (options.html || contentType === "text/html") {
 				const maxX = await this.page.evaluate("Math.max(document.body.scrollWidth, document.body.offsetWidth, document.documentElement.clientWidth, document.documentElement.scrollWidth, document.documentElement.offsetWidth) - window.innerWidth;");
 				const maxY = await this.page.evaluate("Math.max(document.body.scrollHeight, document.body.offsetHeight, document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight) - window.innerHeight;");
 				while (await this.page.evaluate("window.scrollX;") < maxX || await this.page.evaluate("window.scrollY") < maxY) await this.page.evaluate(`window.scrollTo(${maxX}, ${maxY});`);
@@ -33,19 +33,24 @@ class Permaloom {
 	
 			const transactions = (await this.arweave.api.post("/graphql", {query: `query{transactions(sort:HEIGHT_DESC,tags:{name:"page:url",values:["${options.url}"]}){edges{node{tags{value}}}}}`})).data.data.transactions.edges[0];
 			if (!options.after || !transactions || transactions.node.tags[4].value < options.after) {
+				/*
 				const uploader = await this.arweave.transactions.getUploader(await this.arweave.transactions.sign(await this.arweave.createTransaction({
 					data: await res.buffer(),
 					tags: [{"name":"Content-Type", "value":contentType}, {"name":"User-Agent","value":"Permaloom/1.0"}, {"name":"page:url","value":options.url}, {"name":"page:title","value":await this.page.title()}, {"name":"page:timestamp","value":`${Date.now()}`}]
 				}, options.key), options.key));
 				while (!uploader.isComplete) await uploader.uploadChunk();
+				*/
 			}
 			
-			for (j of (await this.page.evaluate("performance.getEntriesByType(\"resource\");")).map(({name}) => name)) {await archive({"url": j, "key": options.key, "hrefs": true, "i": options.i - 1, "after": options.after});}
-			if (hrefs && options.html) {
-				for (j of await this.page.$$eval("a", as => as.map(a => a.href))) {await archive({"url": j, "key": options.key, "i": options.i - 1, "after": options.after, "html": true});}
+			//evaluate("performance.getEntriesByType(\"resource\");") giving wrong response
+			for (let j of (await this.page.evaluate("performance.getEntriesByType(\"resource\");")).map(({name}) => name)) {await this.archive({"url": j, "key": options.key, "i": options.i - 1, "hrefs": options.hrefs, "after": options.after});}
+			if (options.hrefs && options.html) {
+				for (let j of await this.page.$$eval("a", as => as.map(a => a.href))) {await this.archive({"url": j, "key": options.key, "i": options.i - 1, "hrefs": options.hrefs, "after": options.after, "html": true});}
 			}
+
 		}
 	}
+
 };
 
 module.exports = Permaloom;
